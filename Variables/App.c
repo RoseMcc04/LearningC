3c77762f6b5efa0cac57d53bf3119dae1c4dd266
RoseMcc04 <mccormacka@appstate.edu>
2024-07-17 20:04:23 -0400

    displaying examples

--- Variables/App.c
+++ Variables/App.c
@@ -2,5 +2,21 @@
 
 int main() 
 {
-    
+    /*
+    There are four main types of variables in C:
+
+    1. int
+    2. float
+    3. double
+    4. char
+    */
+
+    int x = 7;
+    float y = 16.5;
+
+    // We can print out the integer like this...
+    printf("%d\n", x);
+
+    // We can print out the float like this...
+    printf("%f\n", y);
 }
